8ff770797b028dd5801e8ef2ce6ff164aed7084d
in2erval <in2erval@outlook.com>
2015-11-10 16:33:18 +0000

    Made createFractal function

--- Complex.hs
+++ Complex.hs
@@ -1,17 +1,17 @@
 import Data.Complex
 
-limit = 1000000 --Stop after iteration count reaches limit.
+limit = 100 --Stop after iteration count reaches limit.
 scale :: Int
 scale = 10 -- Between 0 and 1, there are {scale} steps.
 a = 1 -- For generalised NF.
 
 -- Base function
 p :: (RealFloat a) => Complex a -> Complex a
-p z = (z ^ 3) - 1
+p z = sin z
 
 -- Derivative
 p' :: (RealFloat a) => Complex a -> Complex a
-p' z = (3 * (z ^ 2))
+p' z = cos z
 
 newt :: Complex Double -> Int -> Int
 newt val iter 
@@ -25,4 +25,9 @@ createMap x y = rowIter ((-x)*scale) (x*scale) (y*scale) ((-y)*scale)
 rowIter x limX y limY 
 	| x > limX = rowIter (-limX) limX (y - 1) limY
 	| y < limY = []
-	| otherwise = [((fromIntegral x) / (fromIntegral scale), (fromIntegral y) / (fromIntegral scale))] ++ rowIter (x + 1) limX y limY
+	| otherwise = [((fromIntegral x) / (fromIntegral scale), (fromIntegral y)	/ (fromIntegral scale))] ++ rowIter (x + 1) limX y limY
+	
+pairToComplex (a, b) = a :+ b
+
+
+createFractal a b = map (`newt` 0) (map pairToComplex $ createMap a b)
