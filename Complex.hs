adc8048b92a0f38de23d1b3667a5ebafc9b2af91
in2erval <in2erval@outlook.com>
2015-11-10 02:55:57 +0000

    Added Complex.hs

--- Complex.hs
+++ Complex.hs
@@ -1,7 +1,8 @@
 import Data.Complex
 
 limit = 1000000 --Stop after iteration count reaches limit.
-increment = 0.001 -- Increment between complex numbers for evaluation
+increment :: Double
+increment = 0.1 -- Increment between complex numbers for evaluation
 a = 1 -- For generalised NF.
 
 -- Base function
@@ -19,3 +20,9 @@ newt val iter
 	| otherwise 		= newt (func val) (iter + 1)
 	where func zn = (zn - (a * (p zn)/(p' zn)))
 	
+	
+createMap x y = rowIter (-x) x y (-y)
+rowIter x limX y limY 
+	| x > limX = rowIter (-limX) limX (y - increment) limY
+	| y < limY = []
+	| otherwise = [x :+ y] ++ rowIter (x + increment) limX y limY
