f8acd9c5d7f544caca02c4599c6dc775a774d136
in2erval <in2erval@outlook.com>
2015-11-23 10:15:04 +0000

    Added comments to the .hs files.

--- Complex.hs
+++ Complex.hs
@@ -1,14 +1,13 @@
 module Complex
-(newt, newtWithIter, createMap, createFractal, printValues, getRoots) where
+(newt, newtWithIter, createFractal, createFractalWithIter, createFractalWithValues, printValues, getRoots) where
 
 import Data.Complex
 import Data.List
 
-limit = 256 --Stop after iteration count reaches limit.
-scale :: Int
+limit = 256 -- Stop after iteration count reaches limit.
 scale = 10 -- Between 0 and 1, there are {scale} steps.
 a =  1 -- For generalised NF.
-notRoot = 0 :+ 0
+notRoot = 0 :+ 0 -- For filtering out non-converging values.
 
 -- Base function
 p :: (RealFloat a) => Complex a -> Complex a
@@ -24,36 +23,52 @@ p' :: (RealFloat a) => Complex a -> Complex a
 --p' z = 7 * (z ^ 6)
 p' z = 3 * cos z * ((sin z) ^ 2)
 
-newtWithIter :: Complex Double -> Int -> (Int, Complex Double)
+
+func zn = (zn - (a * (p zn)/(p' zn))) -- Newton method.
+
+newt :: Complex Double -> Int -> Complex Double
+newt val iter 
+    | val == func val   = val -- If input value equals func(value), then it reached a root.
+    | iter == limit     = notRoot -- Iteration count reached limit, so we regard it as non-convergeant.
+    | otherwise         = newt (func val) (iter + 1) -- Iterate again, with new value and iteration + 1.
+
+newtWithIter :: Complex Double -> Int -> (Int, Complex Double) -- Same function as newt, but returns (iter, val) instead.
 newtWithIter val iter 
     | val == func val   = (iter, val)
     | iter == limit     = (limit, notRoot)
     | otherwise         = newtWithIter (func val) (iter + 1)
-    where func zn = (zn - (a * (p zn)/(p' zn)))
-
-newt :: Complex Double -> Int -> Complex Double
-newt val iter 
-    | val == func val   = val
-    | iter == limit     = notRoot
-    | otherwise         = newt (func val) (iter + 1)
-    where func zn = (zn - (a * (p zn)/(p' zn)))
-
-getRoots (a, b) = ((filter (/= notRoot)) . nub . (map head) . group . createFractal) (a, b)
 
 
-createMap (x, y) = rowIter ((-x)*scale) (x*scale) (y*scale) ((-y)*scale)
+
+-- (a, b) represents depth in x and depth in y. (1,1) represents a field from (-1 + i) to (1 - i):
+
+--        Im ^ 1
+--           |
+--           |
+--           |
+--           |          1
+-- ----------O---------->
+--           |         Re
+--           |
+--           |
+--           |
+
+createMap (x, y) = rowIter ((-x)*scale) (x*scale) (y*scale) ((-y)*scale)  -- Generates the plane
 rowIter x limX y limY 
     | x > limX = rowIter (-limX) limX (y - 1) limY
     | y < limY = []
-    | otherwise = [((fromIntegral x) / (fromIntegral scale), (fromIntegral y) / (fromIntegral scale))] ++ rowIter (x + 1) limX y limY
+    | otherwise = [((fromIntegral x) / (fromIntegral scale), (fromIntegral y) / (fromIntegral scale))] ++ rowIter (x + 1) limX y limY -- Weird workaround with Int to prevent stupid float inaccuracies.
 
-pairToComplex (a, b) = a :+ b
+pairToComplex (a, b) = a :+ b -- For each value in the plane, represent it as a complex number.
 
-
-createFractal (a, b) = map (`newt` 0) (map pairToComplex $ createMap (a, b))
-createFractal' (a, b) = map (`newtWithIter` 0) (map pairToComplex $ createMap (a, b))
-createFractalWithValues (a, b) = zip (map (`newt` 0) values) values
+createFractal (a, b) = map (`newt` 0) (map pairToComplex $ createMap (a, b)) -- Apply newton method to all the values in the complex plane.
+createFractalWithIter (a, b) = map (`newtWithIter` 0) (map pairToComplex $ createMap (a, b)) -- Same as above, but using newtWithIter instead.
+createFractalWithValues (a, b) = zip (map (`newt` 0) values) values -- Similar to createFractal, but returns a list of (root, initial value).
     where values = (map pairToComplex $ createMap (a, b))
 
-printValues (a, b) = putStr $ unlines $ zipWith (++) (map (f . show) $ map pairToComplex $ createMap (a, b)) (map show $ createFractal' (a, b))
+
+
+getRoots (a, b) = ((filter (/= notRoot)) . nub . (map head) . group . createFractal) (a, b) -- From a given (a, b) field, finds all the different roots that the values converge to.
+
+printValues (a, b) = putStr $ unlines $ zipWith (++) (map (f . show) $ map pairToComplex $ createMap (a, b)) (map show $ createFractalWithIter (a, b)) -- Print the values in ghc to make it more readable.
    where f str = take 20 (str ++ repeat '.')
--- Main.hs
+++ Main.hs
@@ -5,21 +5,23 @@ import Data.Complex
 import Complex
 
 
-scl = 2
-xAdj = 1
-colorList = [red, blue, green, yellow, orange, violet, cyan, magenta, azure, aquamarine, chartreuse, rose]
+scl = 2 -- Zoom level of the generated fractal. Higher values zooms out, values less than 1 zooms in.
+xAdj = 1 -- Translate the picture horizontally. Positive values will move it to the right, Negatives to the left.
+yAdj = 0 -- Same as xAdj, but vertically.
+colorList = [red, blue, green, yellow, orange, violet, cyan, magenta, azure, aquamarine, chartreuse, rose] -- Colour list to match with different roots.
+
+
 
 main :: IO()
-
-main = animateField (InWindow "Newton Fractal" (1000,1000) (400,0)) (1,1) frame
+main = animateField (InWindow "Newton Fractal" (1000,1000) (400,0)) (1,1) frame -- Opens a 1000x1000 window at (400,0) position, with 1 pixel representing each point.
 
 frame :: Float -> Point -> Color
-frame _ (a, b) = f (newtWithIter (g ((scl * a) - xAdj) :+ g (scl * b)) 0)
+frame _ (a, b) = f (newtWithIter (g ((scl * a) - xAdj) :+ g ((scl * b) - yAdj)) 0) -- For each (a,b) point, represent it as a complex number and apply newtWithIter to it.
     where
-    f (iter, x) = dim' iter $ head ([a | (a, b) <- roots, b == x] ++ [black])
-    g = float2Double
+    f (iter, x) = dim' iter $ head ([a | (a, b) <- roots, b == x] ++ [black]) -- Matches all found roots with what newtWithIter gives and colours it accordingly. If none are found, it colours it black.
+    g = float2Double -- The base type is (Complex Double) so needs to convert from Float to Double.
 
-roots = zip ((concat . repeat) colorList) (getRoots (2,2))
+roots = zip ((concat . repeat) colorList) (getRoots (ceiling scl, ceiling scl)) -- Pairs each found root with a colour. getRoots must be bigger than scale.
 
-dim' 0 color = color
-dim' n color = dim' (n `div` 2) (dim color)
+dim' 0 color = color -- Repeated application of dim. 
+dim' n color = dim' (n `div` 2) (dim color) -- Theoretically it dims the colour log2(iter) times.
